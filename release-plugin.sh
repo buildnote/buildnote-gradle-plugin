ed89d588b4a425ed189ec48b63f9cc4ff5bc0a1b
Albert Latacz <albert.latacz@gmail.com>
2024-12-31 09:06:43 +0000

    release script

--- release-plugin.sh
+++ release-plugin.sh
@@ -5,6 +5,4 @@ PROJECT_ENV_FILE="$HOME/dev/buildnote/buildnote.env"
 
 source "$PROJECT_ENV_FILE"
 
-./gradlew :buildnote-gradle-plugin:build
-./gradlew :buildnote-gradle-plugin:shadowJar
-./gradlew :buildnote-gradle-plugin:publishPlugins
+./gradlew build publishPlugins
